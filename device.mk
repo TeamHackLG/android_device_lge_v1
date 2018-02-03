b27e572360d52387625a377b59e5ff431ec7580d
Caio Oliveira <caiooliveirafarias0@gmail.com>
2018-02-03 12:51:29 -0200

    v1: Add device specific variables to start hard unification
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- device.mk
+++ device.mk
@@ -16,7 +16,10 @@
 # This file includes all definitions that apply to ALL L1II devices
 # Everything in this directory will become public
 
+# Define device specific variables to msm7x27a-common
 LGE_MSM7X27A_DEVICE := v1
+LGE_MSM7X27A_GENERATION := 2
+LGE_MSM7X27A_PRODUCT := v1
 
 $(call inherit-product, device/lge/msm7x27a-common/msm7x27a-common.mk)
 $(call inherit-product-if-exists, vendor/lge/v1/v1-vendor.mk)
